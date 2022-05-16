f470efa6ce3ccb77c541ce16b17bb5a8ff6d1495
leo221106 <50825633+leo221106@users.noreply.github.com>
2022-05-16 14:23:49 +0800

    prac7

---
+++ RecursiveBinarySearch.h
@@ -0,0 +1,16 @@
+#ifndef RECURSIVEBINARYSEARCH_H
+#define RECURSIVEBINARYSEARCH_H
+
+#include <iostream>
+#include <stdlib.h>
+#include <vector>
+
+class RecursiveBinarySearch
+{
+public:
+	RecursiveBinarySearch();
+	bool binary_search (std::vector<int> Sort_List);
+	~RecursiveBinarySearch();
+};
+
+#endif //RECURSIVEBINARYSEARCH_H
---
+++ Sort.cpp
@@ -0,0 +1,5 @@
+#include "Sort.h"
+
+Sort::Sort(){}
+
+Sort::~Sort(){}
